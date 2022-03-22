7c254bcbcdbc750414d59667354c9894b73fbdb4
kpekala <kgpekala@gmail.com>
2022-03-22 22:05:20 +0100

    implemented receipt generator

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,46 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.discount.FifteenPercentDiscount;
+import com.virtuslab.internship.discount.TenPercentDiscount;
+import com.virtuslab.internship.product.Product;
+import com.virtuslab.internship.product.ProductDb;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
-        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        List<ReceiptEntry> receiptEntries = generateEntries(basket.getProducts());
+
+        Receipt receipt = new Receipt(receiptEntries);
+
+        applyDiscountIfNecessary(receipt);
+
         return new Receipt(receiptEntries);
+    }
+
+    private void applyDiscountIfNecessary(Receipt receipt) {
+        var tenPercentDiscount = new TenPercentDiscount();
+        var fifteenPercentDiscount = new FifteenPercentDiscount();
+        fifteenPercentDiscount.apply(receipt);
+        tenPercentDiscount.apply(receipt);
+    }
+
+    private List<ReceiptEntry> generateEntries(List<Product> products){
+        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        HashMap<String, Integer> quantities = new HashMap<>();
+        for (var product: products){
+            int lastQuantity = quantities.getOrDefault(product.name(),0);
+            quantities.put(product.name(), lastQuantity + 1);
+        }
+
+        var productDb = new ProductDb();
+        quantities.forEach((productName, quantity) ->{
+            receiptEntries.add(new ReceiptEntry(productDb.getProduct(productName),quantity));
+        });
+        return receiptEntries;
     }
 }
